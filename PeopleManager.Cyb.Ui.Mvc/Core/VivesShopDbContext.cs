c5f65e33c183eba10f29a0bb717ce50b60cbf333
Thybe Van Beersel <thybe.vanbeersel@student.vives.be>
2023-11-12 13:40:13 +0100

    Add items & make checkout page prettier

--- PeopleManager.Cyb.Ui.Mvc/Core/VivesShopDbContext.cs
+++ PeopleManager.Cyb.Ui.Mvc/Core/VivesShopDbContext.cs
@@ -16,6 +16,11 @@ namespace VivesShop.Cyb.Ui.Mvc.Core
             {
                 new ShopItem {ItemName = "Bicky Burger", Price = 4.5f},
                 new ShopItem {ItemName = "Brasilsaus", Price = 1.7f},
+                new ShopItem {ItemName = "Medium friet", Price = 3f},
+                new ShopItem {ItemName = "Frikandel", Price = 2f},
+                new ShopItem {ItemName = "Cola Zero", Price = 2f},
+                new ShopItem {ItemName = "Water", Price = 1.5f},
+                new ShopItem {ItemName = "Mayonaisse", Price = 0.5f},
             });
 
             SaveChanges();
