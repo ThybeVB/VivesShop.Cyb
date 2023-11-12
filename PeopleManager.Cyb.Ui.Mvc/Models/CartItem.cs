b845f55d5868aeac971b091f211d14d8dcd50bee
Thybe Van Beersel <thybe.vanbeersel@student.vives.be>
2023-11-12 13:12:19 +0100

    Fix mvc double type bug

--- PeopleManager.Cyb.Ui.Mvc/Models/CartItem.cs
+++ PeopleManager.Cyb.Ui.Mvc/Models/CartItem.cs
@@ -1,7 +1,17 @@
-﻿namespace VivesShop.Cyb.Ui.Mvc.Models
-{
-    public class CartItem : ShopItem
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace VivesShop.Cyb.Ui.Mvc.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public required string ItemName { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public required float Price { get; set; }
     }
 }
